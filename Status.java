8db9c9f3738b686d98f510e935261ec2ac3cc425
pruthvipatel19 <83479097+pruthvipatel19@users.noreply.github.com>
2023-03-30 18:10:19 -0400

    Create Status.java

---
+++ Status.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FinalProject;
+
+/**
+ *
+ * @author pruth
+ */
+public abstract class Status {
+    public abstract void setSilver(Customer c);
+    
+    public abstract void setGold(Customer c);
+    
+    /*
+     getStatus() and setStatus() should be done in Customer class
+    */
+}
